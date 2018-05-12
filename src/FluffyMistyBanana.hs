20250dca514ebb3552d565ccf3af8a720b4889fd
unknown <mathias.kok@gmail.com>
2018-05-12 23:42:14 +0200

    Exercise 2

--- src/FluffyMistyBanana.hs
+++ src/FluffyMistyBanana.hs
@@ -10,3 +10,8 @@ instance Fluffy [] where
   furry _ [] = []
   furry m (x : xs) = (m x) : (furry m xs)
 
+-- Exercise 2
+-- Relative Difficulty: 1
+instance Fluffy Maybe where
+  furry _ Nothing = Nothing
+  furry m (Just a) = Just $ m a
