4bcacb7af6911f9dd0ff9df6e151e4d429699e2b
unknown <mathias.kok@gmail.com>
2018-05-12 23:41:37 +0200

    Exercise 1

---
+++ src/FluffyMistyBanana.hs
@@ -0,0 +1,12 @@
+module FluffyMistyBanana(module FluffyMistyBanana) where
+
+-- This is clearly identical to the functor typeclass. fmap is not used as this would make the problems trivial.
+class Fluffy f where
+  furry :: (a -> b) -> f a -> f b
+
+-- Exercise 1
+-- Relative Difficulty: 1
+instance Fluffy [] where
+  furry _ [] = []
+  furry m (x : xs) = (m x) : (furry m xs)
+
